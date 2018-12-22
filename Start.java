6e4d0d2f5fb9a15db3e779dc0ccf64d9ff36f1a7
kamilo116 <kamil.sladowski@gmail.com>
2018-12-22 20:26:09 +0100

    lepiej

--- Start.java
+++ Start.java
@@ -14,117 +14,119 @@ import com.sun.corba.se.org.omg.CORBA.ORB;
 
 class OptimizationImpl extends optimizationPOA implements optimizationOperations {
 
-	class SingleServer {
-		private short ip;
-		private int id;
-		private int timeout;
-		private long lastHello;
+    class SingleServer {
+        private short ip;
+        private int id;
+        private int timeout;
+        private long lastHello;
 
-		public SingleServer(int id, short ip, int timeout) {
-			this.id = id;
-			this.ip = ip;
-			this.timeout = timeout;
-		}
-
-
-		public void setTimeout(int timeout) {
-			this.timeout = timeout;
-		}
-
-		public boolean isActive() {
-			return System.currentTimeMillis() - lastHello < timeout;
-		}
-
-		public void activate() {
-			lastHello = System.currentTimeMillis();
-		}
+        public SingleServer(int id, short ip, int timeout) {
+            this.id = id;
+            this.ip = ip;
+            this.timeout = timeout;
+        }
 
 
-	}
+        public void setTimeout(int timeout) {
+            this.timeout = timeout;
+        }
 
-	class SingleServerIpComparator implements Comparator<SingleServer> {
-		@Override
-		public int compare(SingleServer o1, SingleServer o2) {
-			return o1.ip - o2.ip;
-		}
-	}
+        public boolean isActive() {
+            return System.currentTimeMillis() - lastHello < timeout;
+        }
 
-	static AtomicInteger idCount = new AtomicInteger(0);
+        public void activate() {
+            lastHello = System.currentTimeMillis();
+        }
 
-	private ConcurrentHashMap<Integer, SingleServer> idServerMap = new ConcurrentHashMap<>();
-	private ConcurrentHashMap<Short, SingleServer> ipServerMap = new ConcurrentHashMap<>();
-	private ConcurrentSkipListSet<SingleServer> servers = new ConcurrentSkipListSet<SingleServer>(new SingleServerIpComparator());
 
-	@Override
-	public void register(short ip, int timeout, IntHolder id) {
-		SingleServer serverItem = ipServerMap.get(ip);
-		if (serverItem != null) {
-			serverItem.setTimeout(timeout);// czy pottrzebne
-		} else {
-			serverItem = new SingleServer(id.value, ip, timeout);
-			serverItem.activate();
-			ipServerMap.put(ip, serverItem);
-			idServerMap.put(id.value, serverItem);
-			servers.add(serverItem);
-		}
-		id.value = serverItem.id;
-	}
+    }
 
-	@Override
-	public void hello(int id) {
-		SingleServer serverItem = idServerMap.get(id);
-		if (serverItem != null) {
-			serverItem.activate();
-		}
-	}
+    class SingleServerIpComparator implements Comparator<SingleServer> {
+        @Override
+        public int compare(SingleServer o1, SingleServer o2) {
+            return o1.ip - o2.ip;
+        }
+    }
 
-	@Override
-	public void best_range(rangeHolder r) {
-		range bestRange = null, tmpRange = null;
-		Iterator<SingleServer> it = servers.iterator();
-		while (it.hasNext()) {
-			SingleServer sItem = it.next();
-			if (tmpRange == null && sItem.isActive()) {
-				tmpRange = new range(sItem.ip, sItem.ip);
-			} else if (tmpRange != null && sItem.isActive()) {
-				if (sItem.ip - 1 == tmpRange.to) {
-					tmpRange.to += 1;
-				} else {
-					tmpRange = new range(sItem.ip, sItem.ip);
-				}
-			} else {
-				tmpRange = null;
-			}
-			if (bestRange == null || tmpRange != null && tmpRange.to - tmpRange.from > bestRange.to - bestRange.from) {
-				bestRange = tmpRange;
-			}
-		}
-		r.value = bestRange;
-	}
+    static AtomicInteger idCount = new AtomicInteger(0);
+
+    private ConcurrentHashMap<Integer, SingleServer> serversID = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<Short, SingleServer> serversIP = new ConcurrentHashMap<>();
+    private ConcurrentSkipListSet<SingleServer> servers = new ConcurrentSkipListSet<SingleServer>(new SingleServerIpComparator());
+
+    @Override
+    public void register(short ip, int timeout, IntHolder id) {
+        SingleServer serverItem = serversIP.get(ip);
+
+        if (serverItem != null) {
+            serverItem.setTimeout(timeout);// czy pottrzebne
+            id.value = serverItem.id;
+        } else {
+            id.value = idCount.getAndIncrement();
+            serverItem = new SingleServer(id.value, ip, timeout);
+            serverItem.activate();
+            serversIP.put(ip, serverItem);
+            serversID.put(id.value, serverItem);
+            servers.add(serverItem);
+        }
+    }
+
+    @Override
+    public void hello(int id) {
+        SingleServer serverItem = serversID.get(id);
+        if (serverItem != null) {
+            serverItem.activate();
+        }
+    }
+
+    @Override
+    public void best_range(rangeHolder r) {
+        range bestRange = null, tmpRange = null;
+        Iterator<SingleServer> it = servers.iterator();
+        while (it.hasNext()) {
+            SingleServer sItem = it.next();
+            if (tmpRange == null && sItem.isActive()) {
+                tmpRange = new range(sItem.ip, sItem.ip);
+            } else if (tmpRange != null && sItem.isActive()) {
+                if (sItem.ip - 1 == tmpRange.to) {
+                    tmpRange.to += 1;
+                } else {
+                    tmpRange = new range(sItem.ip, sItem.ip);
+                }
+            } else {
+                tmpRange = null;
+            }
+            if (bestRange == null || tmpRange != null && tmpRange.to - tmpRange.from > bestRange.to - bestRange.from) {
+                bestRange = tmpRange;
+            }
+        }
+        r.value = bestRange;
+    }
 }
 
 class Start {
 
-	public static void main(String[] args) {
-		try {
-			org.omg.CORBA.ORB orb = ORB.init(args, null);
-			POA rootpoa = (POA) orb.resolve_initial_references("RootPOA");
-			rootpoa.the_POAManager().activate();
+    public static void main(String[] args) {
+        try {
+            org.omg.CORBA.ORB orb = ORB.init(args, null);
+            POA rootpoa = (POA) orb.resolve_initial_references("RootPOA");
+            rootpoa.the_POAManager().activate();
 
-			OptimizationImpl optimizationImpl = new OptimizationImpl();
-			org.omg.CORBA.Object ref = rootpoa.servant_to_reference(optimizationImpl);
+            OptimizationImpl optimizationImpl = new OptimizationImpl();
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(optimizationImpl);
 
-			System.out.println(orb.object_to_string(ref));
+            System.out.println(orb.object_to_string(ref));
 
-			org.omg.CORBA.Object namingContextObj = orb.resolve_initial_references("NameService");
-			NamingContext nCont = NamingContextHelper.narrow(namingContextObj);
-			NameComponent[] path = { new NameComponent("Optymalizacja", "Object") };
+            org.omg.CORBA.Object namingContextObj = orb.resolve_initial_references("NameService");
+            NamingContext nCont = NamingContextHelper.narrow(namingContextObj);
+            NameComponent[] path = { new NameComponent("Optymalizacja", "Object") };
 
-			nCont.rebind(path, ref);
-			orb.run();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            nCont.rebind(path, ref);
+            orb.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 }
